1ddabc287f3717ee76ed6c37a13947c546d089f6
6691a <6691a@naver.com>
2022-04-18 23:27:35 +0900

    CHORE : exchange_rate app task.py name error -> tasks.py

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 
-app = Celery('config', include=['exchange_rate.tasks'])
+app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()

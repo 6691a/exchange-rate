f1cfa8abb9d232dee781c94a3db8bbf660d46d0c
6691a <6691a@naver.com>
2022-04-20 17:49:52 +0900

    TEST : celery beat test

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -19,15 +19,16 @@ app.conf.beat_schedule = {
 		'task': 'exchange_rate.tasks.update_exchange_rate',
 		'schedule': crontab(hour='9-15', minute='*/5', day_of_week='1-5'),
 	},
-	# 15:00 ~ 15:30
+	# # 15:00 ~ 15:30
 	# 'last_update_exchange_rate': {
 	# 	'task': 'exchange_rate.tasks.update_exchange_rate',
-	# 	'schedule': crontab(hour='15', minute='*/5,0-30', day_of_week='1-5'),
+	# 	'schedule': crontab(hour='15', minute='31-59/2', day_of_week='1-5'),
 	# },
+	
 
 	'last_update_exchange_rate': {
 		'task': 'exchange_rate.tasks.test',
-		'schedule': crontab(hour='17', minute='31-59*/5', day_of_week='1-5'),
+		'schedule': crontab(hour='17', minute='31-59/2', day_of_week='1-5'),
 	},
 }
 

09039051d02add65135ec3a5cf6c270caddf64c4
6691a <6691a@naver.com>
2022-04-20 12:13:45 +0900

    TEST :celery test

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -1,6 +1,7 @@
 from .settings import *
 from pydantic import BaseSettings, SecretStr
 
+
 class Env(BaseSettings):
     # DB
     DB_USERNAME: str
@@ -8,18 +9,14 @@ class Env(BaseSettings):
     DB_HOST: str
     DB_PORT: str
     DB_NAME: str
-
+    
     # Django
     SECRET_KEY: SecretStr
     TIME_ZONE: str
-    EXCHANGE_RATE_API_URL: str
-    
-    CELERY_BROKER_URL: SecretStr 
 
     class Config:
         env_file = "dev.env"
         env_file_encoding = "utf-8"
-
 
 
 env = Env()
@@ -32,11 +29,11 @@ ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS += [
+    'django_celery_beat',
+    'django_celery_results',
 ]
 
 MIDDLEWARE += [
-    'django_celery_beat',
-    'django_celery_results',
 ]
 
 DATABASES = {
@@ -50,14 +47,3 @@ DATABASES = {
 }
 
 TIME_ZONE = env.TIME_ZONE
-
-# celery settings
-CELERY_ALWAYS_EAGER = True
-# CELERY_BROKER_URL = 'amqp://[user_name]:[password]@localhost/[vhost_name]'
-CELERY_BROKER_URL = env.CELERY_BROKER_URL.get_secret_value()
-CELERY_RESULT_BACKEND = 'django-db'
-# CELERY_CACHE_BACKEND = 'django-cache'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = env.TIME_ZONE

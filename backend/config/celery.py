1b313db2eb5144b89d3211d475f3e00faacd12ec
6691a <6691a@naver.com>
2022-04-22 15:43:22 +0900

    CHORE : jenkins file back

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -13,7 +13,15 @@ app.autodiscover_tasks()
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
-# app.conf.beat_schedule = {
+app.conf.beat_schedule = {
+    "TEST1": {
+        "task": "exchange_rate.tasks.day_off",
+        "schedule": crontab(minute="30-59/1", day_of_week="1-5"),
+    },
+    "TEST2": {
+        "task": "exchange_rate.tasks.day_off",
+        "schedule": crontab(hour='9-15', minute='*/1', day_of_week="1-5"),
+    },
 #     "day_off": {
 #         "task": "exchange_rate.tasks.day_off",
 #         "schedule": crontab(hour="8", minute="30", day_of_week="1-5"),
@@ -28,5 +36,5 @@ def debug_task(self):
 # 		'task': 'exchange_rate.tasks.update_exchange_rate',
 # 		'schedule': crontab(hour='15', minute='0-30/5', day_of_week='1-5'),
 # 	},
-# }
+}
 

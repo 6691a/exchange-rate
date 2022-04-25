5b88816afe612826c7b5449de32178a8c5e92647
6691a <6691a@naver.com>
2022-04-25 16:54:14 +0900

    TSET : ninja test

--- backend/exchange_rate/apis/v1/apis.py
+++ backend/exchange_rate/apis/v1/apis.py
@@ -8,8 +8,13 @@ router = Router()
 
 # def _():
 
+# @router.get("", response=list[ExchangeRateSchema])
+# async def get_exchange_rate(request, currency: str):
+#     test = await sync_to_async(list)(ExchangeRate.objects.filter(currency__icontains=currency))
+#     return test
+#     # return {"message": "Hello from V1"}
+
 @router.get("", response=list[ExchangeRateSchema])
-async def get_exchange_rate(request, currency: str):
-    test = await sync_to_async(list)(ExchangeRate.objects.filter(currency__icontains=currency))
+def get_exchange_rate(request, currency: str):
+    test = ExchangeRate.objects.filter(currency__icontains=currency)
     return test
-    # return {"message": "Hello from V1"}

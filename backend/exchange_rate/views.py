4f4bc78f46e9c5f306ba707b1a48bc9af6b6a4f3
6691a <6691a@naver.com>
2022-05-07 15:22:22 +0900

    TEST : chennals test

--- backend/exchange_rate/views.py
+++ backend/exchange_rate/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
+# @login_required
 def main(request):
-    return render(request, "main.html")
-
-
-from channels.layers import get_channel_layer
-
-channel_layer = get_channel_layer()
-
-
-def channels(request):
-    # channel_layer.group_send(
-    #     "USA", {
-    #         type:
-    #         "text":
-    #     }
-    # )
-    # print(channel_layer)
     return render(request, "main.html")

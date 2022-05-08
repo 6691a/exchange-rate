963c061e447514fb8cf85df1aa2ec7cea3b34a1c
6691a <6691a@naver.com>
2022-05-08 12:14:00 +0900

    TEST : nginx socket setting

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -13,8 +13,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from exchange_rate.channel.routing import websocket_urlpatterns
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+
+django.setup()
+
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(

2de644e4ed3e803a4b06e620449cff97a1293856
6691a <6691a@naver.com>
2022-05-19 17:51:28 +0900

    FEAT : channel data model create and connect, disconnect trackking

--- backend/exchange_rate/channel/consumers.py
+++ backend/exchange_rate/channel/consumers.py
@@ -4,6 +4,7 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 
 from base.schemas import ResponseSchema, ErrorSchema
+from channel.models import Channel
 from ..apis.v1.schemas import ExchangeRateSchema
 from ..models import ExchangeRate
 
@@ -14,6 +15,16 @@ def today_exchange(*args, **kwargs) -> List:
     return list(ExchangeRate.objects.filter(*args, **kwargs))
 
 
+@database_sync_to_async
+def save_channel_data(user, group_name, channel_name) -> None:
+    Channel.objects.create(user=user, group_name=group_name, channel_name=channel_name)
+
+
+@database_sync_to_async
+def delete_channel_data(group_name, channel_name) -> None:
+    Channel.objects.get(group_name=group_name, channel_name=channel_name).delete()
+
+
 class ExchangeRateConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         """
@@ -22,15 +33,16 @@ class ExchangeRateConsumer(AsyncJsonWebsocketConsumer):
         other :
             await self.send_json()
         """
-        self.user = self.scope["user"]
+        user = self.scope["user"]
         self.group_name = self.scope["url_route"]["kwargs"]["currency"]
-        
-        print(self.user)
-        print(self.group_name)
-        print(self.channel_name)
+
+        if not user.is_authenticated:
+            return
 
         await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await save_channel_data(user, self.group_name, self.channel_name)
         await self.accept()
+
         if exchange := await today_exchange(currency__icontains=self.group_name):
             return await self.send(
                 ResponseSchema(data=[ExchangeRateSchema(**i.dict) for i in exchange]).json()
@@ -40,4 +52,5 @@ class ExchangeRateConsumer(AsyncJsonWebsocketConsumer):
         )
 
     async def disconnect(self, close_code):
+        await delete_channel_data(self.group_name, self.channel_name)
         await self.channel_layer.group_discard(self.group_name, self.channel_name)

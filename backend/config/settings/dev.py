0fa93b8b053cc15448c0676a587c0aad32a1a1fb
6691a <6691a@naver.com>
2022-06-24 10:33:17 +0900

    CI : sqlite3 timeout 30

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -53,7 +53,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        "timeout": 20,
+        "timeout": 30,
     }
 }
 

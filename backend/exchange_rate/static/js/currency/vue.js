6ae5e713046d18b4fc0fd2e7a5113c9e240be5a4
6691a <6691a@naver.com>
2022-09-08 14:46:45 +0900

    FEAT: FEAT: front chart code name change

--- backend/exchange_rate/static/js/currency/vue.js
+++ backend/exchange_rate/static/js/currency/vue.js
@@ -6,31 +6,16 @@ import {alertFuncs, alertVars} from "../../../static/js/alert/alert.js";
 const indexVue = Vue.createApp({
     delimiters: ['[[', ']]'],
     setup() {
-        const currency = window.location.pathname.split("/").pop();
-        const series = [{
-            name: '가격',
-            data: [],
-        }];
-        const apexChart = Vue.ref();
-        Vue.onBeforeMount(async () => {
-            const res = await chartFuncs.get_exchange_rate(currency)
-            const exchange = res.data.data.exchange_rate
-            exchange.forEach((v) => series[0].data.push({ x: v.created_at, y: v.standard_price }))
-            apexChart.value.updateSeries(series)
-        });
-        Vue.onMounted(async () => {
-            chartFuncs.renderChart(series, apexChart)
-
-        });
         return {
-            currency,
-            series,
-            apexChart,
             watchFuncs,
             alertFuncs,
             chartVars,
             alertVars,
         }
+    },
+    mounted() {
+        chartFuncs.renderChart()
+        chartFuncs.socketConnect(window.location.pathname.split("/").pop())
     },
 
 })
